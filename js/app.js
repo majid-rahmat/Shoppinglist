21ba8c93fcadc73942f4eaa700110f6d86056e0e
majid-rahmat <majid.rahmat@gmail.com>
2016-06-13 23:45:35 -0400

    empty

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	$('button').click(function() {
 		$('ul').append('<li>' + $('input').val() + '<button class="line-through">X</button><button class="remove">Remove</button></li>');
 	});
-	$('ul').on('click','.line-through',function() {
-		$(this).parent().css('text-decoration','line-through');
-	});
 	$('ul').on('click','.remove',function() {
 		$(this).parent().remove();
+	});
+	$('ul').on('click','.line-through',function() {
+		$(this).parent().css('text-decoration','line-through');
 	});
 });
